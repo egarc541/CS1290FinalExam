348f15398b7627dadb411f0b9bea6eabcf363c92
egarc541 <egarcia88@utep.edu>
2018-12-13 05:31:33 -0700

    Update IntegerBreak.java

--- Integer Break/IntegerBreak.java
+++ Integer Break/IntegerBreak.java
@@ -3,10 +3,10 @@ public class IntegerBreak {
   
   int [] dp = new int [n + 1];
   dp[1] = 1;
-  
+  int maxdp = 0;
   for(int i = 2; i < n ; i++){
     for(int j = 1; j <= i / 2; j++){
-     int maxdp = dp[i]; 
+    maxdp = dp[i]; 
     maxdp = Math.max(Math.max(dp[j],j) * Math.max(i-j, dp[i-j]),dp[i);
     }
   }
